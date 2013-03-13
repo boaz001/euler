48839d3f00965fd1ef7d4e3b7e0953bbc879d6df
Boaz Stolk <boaz.stolk@gmail.com>
2013-03-13 12:23:03 +0530

    changed function name and call it at the end

--- Euler 1.py
+++ Euler 1.py
@@ -1,7 +1,7 @@
 # Project Euler problem 1
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
-def sm(n):
+def prob1(n):
   i = 0
   sum = 0
   additosum = 0
@@ -15,3 +15,5 @@ def sm(n):
       additosum = 0
     i = i + 1
   return sum
+
+prob1(1000)
